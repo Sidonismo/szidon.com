479a5a024dd418f894910cb492f6f7c1868b94ba
Debian <bitnami@ip-172-26-1-130.eu-central-1.compute.internal>
2020-11-20 12:15:42 +0000

    změna send

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Haló, haló, prase kozu potrkalo');
 });
 
 module.exports = router;
